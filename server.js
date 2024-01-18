80bad68597218178cc5f89b80f913ec62395c0c7
Rafael Gugelmin <rafaelgug@gmail.com>
2024-01-17 21:15:34 -0300

    Instalado nodemon - npm install --save-dev nodemon

--- server.js
+++ server.js
@@ -9,4 +9,21 @@ app.get('/', (req, res) => {
 
 })
 
+
+app.get('/devices', (req, res) => {
+    const devices = [{
+        id: 123,
+        nome: "Geledeira",
+        hwh: 23,
+        corrente: 2.1,
+        voltagem: 127,
+        fp: 1
+    }]
+    res.json({
+        success: true,
+        deveces: devices
+    })
+
+})
+
 app.listen(PORT)
