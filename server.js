70a56973858fcdcda38326ddbc3cfe184baf2ad7
Rafael Gugelmin <rafaelgug@gmail.com>
2024-01-18 08:29:30 -0300

    Implementado bodyparser para interpretar json metodo post

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
+
 const api = require('./rotas/')
 app.use('/api', api)
 const PORT = 3000
